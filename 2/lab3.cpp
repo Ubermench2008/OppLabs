c143405480a2cb260b758a8ffddfa3d03f2712a2
Ubermench2008 <i.danilov@g.nsu.ru>
2025-03-20 20:26:29 +0700

    Update lab3.cpp

--- 2/lab3.cpp
+++ 2/lab3.cpp
@@ -1,4 +1,14 @@
 #include "utils/utils.h"
+#include <mpi.h>
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <cmath>
+#include <cblas.h>
+#include <iomanip>
+#include <random>
+
+
 
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
@@ -7,7 +17,7 @@ int main(int argc, char** argv) {
     int size, rank;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    
+
     if (argc == 3) {
         dims[0] = std::atoi(argv[1]);
         dims[1] = std::atoi(argv[2]);
@@ -24,7 +34,7 @@ int main(int argc, char** argv) {
         MPI_Finalize();
         return 1;
     }
-    
+
     MPI_Comm gridComm, columnComm, rowComm;
     MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, reorder, &gridComm);
     int coords[2];
@@ -37,18 +47,18 @@ int main(int argc, char** argv) {
     MPI_Cart_sub(gridComm, subDims, &columnComm);
 
     int sub_n = n1 / dims[0];
-    int sub_m = n3 / dims[1];
-    
+    int sub_m = n3 / dims[1]; 
+
     std::vector<double> subA(sub_n * n2);
     std::vector<double> subB(n2 * sub_m);
     std::vector<double> subC(sub_n * sub_m);
-    
+
     std::vector<double> A, B, C;
     if (coords[0] == 0 && coords[1] == 0) {
         A.resize(n1 * n2);
         B.resize(n2 * n3);
         C.resize(n1 * n3);
-        
+
         fillMatrixRandom(A, n1 * n2);
         fillMatrixRandom(B, n2 * n3);
 
@@ -65,31 +75,24 @@ int main(int argc, char** argv) {
     }
     MPI_Bcast(subA.data(), sub_n * n2, MPI_DOUBLE, 0, rowComm);
     MPI_Type_free(&SUB_A);
-    
+
+    MPI_Datatype SUB_B, SUB_B_RESIZED;
+    MPI_Type_vector(n2, sub_m, n3, MPI_DOUBLE, &SUB_B);
+    MPI_Type_create_resized(SUB_B, 0, sub_m * sizeof(double), &SUB_B_RESIZED);
+    MPI_Type_commit(&SUB_B_RESIZED);
+    MPI_Type_free(&SUB_B);
+
     if (coords[0] == 0) {
-        std::vector<double> B_packed;
-        if (coords[1] == 0) {
-            B_packed.resize(n2 * n3); // n3 = dims[1] * sub_m
-            for (int i = 0; i < dims[1]; i++) {
-                for (int row = 0; row < n2; row++) {
-                    std::copy(B.begin() + row * n3 + i * sub_m,
-                              B.begin() + row * n3 + i * sub_m + sub_m,
-                              B_packed.begin() + i * (n2 * sub_m) + row * sub_m);
-                }
-            }
-        }
-        MPI_Scatter((coords[1] == 0 ? B_packed.data() : nullptr),
-                    n2 * sub_m, MPI_DOUBLE,
+        MPI_Scatter(B.data(), 1, SUB_B_RESIZED,
                     subB.data(), n2 * sub_m, MPI_DOUBLE, 0, rowComm);
     }
     MPI_Bcast(subB.data(), n2 * sub_m, MPI_DOUBLE, 0, columnComm);
-    
+
     for (int row = 0; row < sub_n; row++) {
         for (int col = 0; col < sub_m; col++) {
             subC[row * sub_m + col] = 0.0;
         }
     }
-
     for (int row = 0; row < sub_n; row++) {
         for (int i = 0; i < n2; i++) {
             double a_val = subA[row * n2 + i];
@@ -99,41 +102,29 @@ int main(int argc, char** argv) {
         }
     }
 
+    MPI_Datatype SUB_C, SUB_C_RESIZED;
+    int sizes[2]    = { n1, n3 };
+    int subsizes[2] = { sub_n, sub_m };
+    int starts[2]   = { 0, 0 };
+    MPI_Type_create_subarray(2, sizes, subsizes, starts, MPI_ORDER_C, MPI_DOUBLE, &SUB_C);
+    MPI_Type_create_resized(SUB_C, 0, sub_n * sub_m * sizeof(double), &SUB_C_RESIZED);
+    MPI_Type_commit(&SUB_C_RESIZED);
+    MPI_Type_free(&SUB_C);
+
     int localCount = sub_n * sub_m;
     int totalBlocks = dims[0] * dims[1];
-    
-    std::vector<int> recvcounts;
-    std::vector<int> displs;
-    if (coords[0] == 0 && coords[1] == 0) {
-        recvcounts.resize(totalBlocks, localCount);
-        displs.resize(totalBlocks, 0);
+    std::vector recvcounts(totalBlocks, 1);
+    std::vector displs(totalBlocks, 0);
+    for (int proc = 0; proc < totalBlocks; proc++) {
+        displs[proc] = proc;
+    }
 
-        for (int proc = 0; proc < totalBlocks; proc++) {
-            displs[proc] = proc * localCount;
-        }
-    }
-    std::vector<double> tmpC;
-    if (coords[0] == 0 && coords[1] == 0) {
-        tmpC.resize(totalBlocks * localCount);
-    }
     MPI_Gatherv(subC.data(), localCount, MPI_DOUBLE,
-                (coords[0] == 0 && coords[1] == 0 ? tmpC.data() : nullptr),
-                recvcounts.data(), displs.data(), MPI_DOUBLE,
+                (coords[0] == 0 && coords[1] == 0 ? C.data() : nullptr),
+                recvcounts.data(), displs.data(), SUB_C_RESIZED,
                 0, gridComm);
 
-    if (coords[0] == 0 && coords[1] == 0) {
-        for (int proc = 0; proc < totalBlocks; proc++) {
-            int proc_row = proc / dims[1]; 
-            int proc_col = proc % dims[1]; 
-            for (int i = 0; i < sub_n; i++) {
-                for (int j = 0; j < sub_m; j++) {
-                    int global_row = proc_row * sub_n + i;
-                    int global_col = proc_col * sub_m + j;
-                    C[global_row * n3 + global_col] = tmpC[proc * localCount + i * sub_m + j];
-                }
-            }
-        }
-    }
+    MPI_Type_free(&SUB_C_RESIZED);
 
     if (rank == 0) {
         endTime = MPI_Wtime();
